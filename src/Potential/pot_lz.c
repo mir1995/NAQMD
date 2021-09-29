573a362142cb68f718ce9e1da6b4d6880d90a455
Michael <mir00021@hotmail.com>
2021-09-29 10:10:00 +0100

    modifying input to construction of potential

--- src/Potential/pot_lz.c
+++ src/Potential/pot_lz.c
@@ -8,68 +8,46 @@
 
 
 
-static double v12(double x, double delta){
-  return delta;
-}
-
-static double v12d(double x){
-  return 0;
-}
 
 /* Handles construction of potential in the adiabatic representation */
 
-static double z(double x, double alpha){
-  return alpha * x;
-}
-
-static double zd(double x, double alpha){
-  return alpha;
-}
-
-double d(struct Potential *pot, double *x, unsigned int dim){
-  return 0.0;
-}
-
-static double dd(double *x){
-  return 0.0;
-}
-
-double rho(struct Potential *pot, double *x, unsigned int dim){
-  return sqrt(pow(pot->alpha * x[0],2) + pow(pot->delta,2));
+double v_z(struct Potential *pot, double *x, unsigned int dim){
+  return pot->alpha * x[0];
 }
 
 double v_zd(struct Potential *pot, double *x, unsigned int dim){
   return pot->alpha;
 }
 
+double v_zdd(struct Potential *pot, double *x, unsigned int dim){
+  return 0.0;
+}
+
+double v_v12(struct Potential *pot, double *x, unsigned int dim){
+  return pot->delta;
+}
+
 double v_v12d(struct Potential *pot, double *x, unsigned int dim){
   return 0.0;
 }
 
-void grad_v_up(struct Potential *pot, double *grad_v, double *x, unsigned int dim){
-  for(unsigned int i=0; i<dim; i++){
-    grad_v[i] = (z(x[0], pot->alpha) * zd(x[0], pot->alpha) + v12(x[0], pot->delta) * v12d(x[0])) / rho(pot, x, dim) + dd(x);
-  }
+double v_v12dd(struct Potential *pot, double *x, unsigned int dim){
+  return 0.0;
 }
 
-void grad_v_down(struct Potential *pot, double *grad_v, double *x, unsigned int dim){
+double v_trace(struct Potential *pot, double *x, unsigned int dim){
+  return 0.0;
+}
 
-  for(unsigned int i=0; i<dim; i++){
-    grad_v[i] = - (z(x[0], pot->alpha) * zd(x[0], pot->alpha) +\
-        v12(x[0], pot->delta) * v12d(x[0])) / rho(pot, x, dim) + dd(x);
-  }
+double v_traced(struct Potential *pot, double *x, unsigned int dim){
+  return 0.0;
 }
 
 double get_tau(struct Potential *pot){
-  // the value of tau has been computed separately 
-  return 0.;
+  return M_PI * pow(pot->delta,2) / 2 / pot->alpha;
 }
 // the following are still to compute
-void dd_v_up(struct Potential *pot, double *dd_v, double *x, unsigned int dim){
-  return 0;
-}
+void dd_v_up(struct Potential *pot, double *dd_v, double *x, unsigned int dim){}
 
-void dd_v_down(struct Potential *pot, double *dd_v, double *x, unsigned int dim){
-  return 0;
-}
+void dd_v_down(struct Potential *pot, double *dd_v, double *x, unsigned int dim){}
 
