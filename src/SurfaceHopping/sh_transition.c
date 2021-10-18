368656391889efb3fd3879e186cada9ec2a2ce71
Michael <mir00021@hotmail.com>
2021-10-18 17:23:00 +0100

    Included prefactor in the superadiabatic transition rate + renamed transition rate more meaninggully

--- src/SurfaceHopping/sh_transition.c
+++ src/SurfaceHopping/sh_transition.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include "surface_hopping.h"
 
-double sh_transition_belayev(struct Particle *particle, struct Potential *potential,
+// add a reference to the paper equation number
+double sh_transition_lzadia(struct Particle *particle, struct Potential *potential,
                               struct Odeint *odeint){
   double k = sqrt(pow(particle->p_curr[0],2) * (pow(potential->func_zd(potential, particle->x_curr, 1), 2) + \
              pow(potential->func_v12d(potential, particle->x_curr, 1), 2) ) + pow(particle->p_curr[0], 2) *\
@@ -15,12 +16,7 @@ double sh_transition_belayev(struct Particle *particle, struct Potential *potent
   return exp(- M_PI / potential->eps * pow(potential->delta,2) / k ); 
 }
 
-double sh_transition_landau(struct Particle *particle, struct Potential *potential,
-                            struct Odeint *odeint){
-  return 0.5;
-}
-
-double sh_transition_lasser(struct Particle *particle, struct Potential *potential,
+double sh_transition_lzdia(struct Particle *particle, struct Potential *potential,
                             struct Odeint *odeint){
   double k = sqrt(pow(potential->func_zd(potential, particle->x_curr, 1), 2) + \
              pow(potential->func_v12d(potential, particle->x_curr, 1), 2)) \
@@ -28,29 +24,63 @@ double sh_transition_lasser(struct Particle *particle, struct Potential *potenti
   return exp(- M_PI / potential->eps * pow(potential->delta,2) / k);
 }
 
-double sh_transition_goddard(struct Particle *particle, struct Potential *potential,
+double sh_transition_sa(struct Particle *particle, struct Potential *potential,
                             struct Odeint *odeint){
+  // there will be a minus sign if transitioning from top to bottom
+  // you could use the status of the particle for that
+  // there could also be a sign inside of b
   double tau = potential->func_get_tau(potential);
-  // is there a minus if we are transitioning from bottom up?
   double k = sqrt(pow(particle->p_curr[0], 2) + 4*potential->delta);
-  return exp(- tau/(potential->delta*potential->eps) * fabs(k - particle->p_curr[0]));
+  double b = pow((k + particle->p_curr[0]) / 2 / particle->p_curr[0], 2);
+  return b * exp(- tau/(potential->delta*potential->eps) * fabs(k - particle->p_curr[0]));
 }
 
-double sh_transition_goddard1(struct Particle *particle, struct Potential *potential,
+double sh_transition_sa1(struct Particle *particle, struct Potential *potential,
                             struct Odeint *odeint){
   double tau = M_PI * pow(potential->delta,2) / 2 / potential->alpha; // this is an approximate computation of tau
   double k = sqrt(pow(particle->p_curr[0], 2) + 4*potential->delta);
-  return exp(- tau/(potential->delta*potential->eps) * fabs(k - particle->p_curr[0]));
+  double b = pow((k + particle->p_curr[0]) / 2 / particle->p_curr[0], 2);
+  return b * exp(- tau/(potential->delta*potential->eps) * fabs(k - particle->p_curr[0]));
 }
 
-double sh_transition_goddard2(struct Particle *particle, struct Potential *potential,
+double sh_transition_sa2(struct Particle *particle, struct Potential *potential,
                             struct Odeint *odeint){
   double tau = potential->func_get_tau(potential);
-  return exp(- tau/(potential->delta*potential->eps) * \
+  double k = sqrt(pow(particle->p_curr[0], 2) + 4*potential->delta);
+  double b = pow((k + particle->p_curr[0]) / 2 / particle->p_curr[0], 2);
+  return b * exp(- tau/(potential->delta*potential->eps) * \
       fabs(2*potential->delta / particle->p_curr[0]));
 }
 
-double sh_transition_goddard3(struct Particle *particle, struct Potential *potential,
+double sh_transition_sa3(struct Particle *particle, struct Potential *potential,
+                            struct Odeint *odeint){
+  double tau = potential->func_get_tau(potential);
+  double k = sqrt(pow(particle->p_curr[0], 2) + 4*potential->delta);
+  return exp(- tau/(potential->delta*potential->eps) * fabs(k - particle->p_curr[0]));
+}
+
+double sh_transition_sa12(struct Particle *particle, struct Potential *potential,
+                            struct Odeint *odeint){
+  double tau = M_PI * pow(potential->delta,2) / 2 / potential->alpha;
+  double k = sqrt(pow(particle->p_curr[0], 2) + 4*potential->delta);
+  double b = pow((k + particle->p_curr[0]) / 2 / particle->p_curr[0], 2);
+  return b * exp(- tau/(potential->delta*potential->eps) * fabs(2*potential->delta / particle->p_curr[0]));
+}
+
+double sh_transition_sa13(struct Particle *particle, struct Potential *potential,
+                            struct Odeint *odeint){
+  double tau = M_PI * pow(potential->delta,2) / 2 / potential->alpha;
+  double k = sqrt(pow(particle->p_curr[0], 2) + 4*potential->delta);
+  return exp(- tau/(potential->delta*potential->eps) * fabs(k - particle->p_curr[0]));
+}
+
+double sh_transition_sa23(struct Particle *particle, struct Potential *potential,
+                            struct Odeint *odeint){
+  double tau = potential->func_get_tau(potential);
+  return exp(- tau/(potential->delta*potential->eps) * fabs(2*potential->delta / particle->p_curr[0]));
+}
+
+double sh_transition_sa123(struct Particle *particle, struct Potential *potential,
                             struct Odeint *odeint){
   double tau = M_PI * pow(potential->delta,2) / 2 / potential->alpha;
   return exp(- tau/(potential->delta*potential->eps) * fabs(2*potential->delta / particle->p_curr[0]));
