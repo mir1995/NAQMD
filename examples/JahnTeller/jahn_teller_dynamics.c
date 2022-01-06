1599ed9f7f6162b62c85ebaecfef640a3642d71b
Michael <mir00021@hotmail.com>
2022-01-06 19:39:48 +0100

    jahn teller multidimensional rate

--- examples/JahnTeller/jahn_teller_dynamics.c
+++ examples/JahnTeller/jahn_teller_dynamics.c
@@ -9,19 +9,18 @@
 #include <time.h> // srand(time(0))
 #include "../../setup.h" 
 
-#define GAMMA 0
+
+#define GAMMA 0 
 #define EPS 0.01
-#define DIM 2
 
 int main(int argc, char *argv[]){
     
   /*
    * INITIALISE PARAMETERS
    */
-  unsigned int npart, dim;
-  int t, s;
-  double dt;
-  double q[DIM], p[DIM];
+  long int npart;
+  int dim, t, s;
+  double dt, q[DIM], p[DIM];
   double param[4];
   FILE *file;
   
@@ -30,46 +29,39 @@ int main(int argc, char *argv[]){
    */
   
   dim = DIM; 
-  t = 6, dt = 0.001;
-  q[0]=5*sqrt(EPS), q[1]=0.5*sqrt(EPS), p[0]=0, p[1]=0;
-  npart = atoi(argv[1]); // given that you know the convergence rate of the 
-  // two methods, what is the corresponding number of points that 
-  // accuracy
-  s = 1;
+  t = 2, dt = 0.01;
+  q[0] = 5*sqrt(EPS), q[1] = 0.5*sqrt(EPS), p[0]=0, p[1]=0;
+  npart = pow(10,6);
+  s = atoi(argv[2]);
   param[0] = EPS;
   param[3] = GAMMA;
-  char *rate = argv[2];
+  char *rate = argv[1];
   /*
    *  PRINT SIMULATION PARAMETERS
   */
 
   printf(" ***************************************************************\n");
-  printf(" Jahn Teller: Probabilistic Single Switch Surface Hopping  \n");
-  printf("---> NPART     :  %d\n",npart);
+  printf(" Jahn Teller: Single Switch Surface Hopping  \n");
+  printf("---> NPART     :  %ld\n",npart);
   printf("---> DIM       :  %d\n",dim);
   printf("---> T         :  %d\n", t);
   printf("---> dt        :  %.4f\n", dt);
-  printf("---> q         :  %.4f\n", q[0]);
-  printf("---> p         :  %.4f\n", p[0]);
-  printf("---> POTENTIAL :  Jahn Teller \n"); 
-  printf("---> RATE      :  %s\n", rate);
+  printf("---> POTENTIAL :  JahnTeller \n"); 
   printf(" ***************************************************************\n");
 
   /*
    *  GENERATE ARRAY FOR PARTICLES, INITIALISE SOLVER AND POTENTIAL
    */
-  struct Particle *particles = sh_particles_create(npart, dim); // i think there is actually no need for the pointer 
-  
+  struct Particle *particles = sh_particles_create(npart, dim); // it's a weird data struct - to improve
   struct Odeint *solver = odeint_new(t, dt, dim, "lietrotter_symplectic");
-  // thinking I do not need to pass this as pointer
   struct Potential *pot = potential_construct(&v_trace, &v_z, &v_v12, &v_traced, 
                                               &v_zd, &v_v12d, &v_zdd, &v_v12dd,
-                                              &get_tau, "Jahn Teller", param);
+                                              &get_tau, "JahnTeller", param);
   struct Observables *observables = sh_observables_new(npart, dim);
   struct Hopper *hopper = sh_hopper_new(rate);
 
   char filename[200];
-  sprintf(filename, "./data/observables_gamma%dseed%d_npart%d_rate%s.txt", GAMMA, s, npart, rate); 
+  sprintf(filename, "./data/observables_npart%ld_gamma%d_rate%s_seed%d.txt", npart, (int) GAMMA, rate, s); 
   file = fopen(filename, "w"); 
 
 
@@ -83,6 +75,7 @@ int main(int argc, char *argv[]){
 
   srand(s); // INITIALISE SEED // what is the actual variance...?
   sh_wigner_fill(particles, q, p, sqrt(EPS/2), npart, dim);
+  
   sh_particle_potential_init(particles, pot, npart, dim);// initialise particle values - potential, gradient, level ...
 
   /*
@@ -94,14 +87,15 @@ int main(int argc, char *argv[]){
   printf("   Surface Particle Hopping Simulation  \n");
   printf(" ***************************************************************\n");
   
-  fprintf(file, "itr \t x_up[0] \t x_up[1] \t x_down[0] \t x_down[1] \t \
-      p_up[0] \t p_up[1] \t p_down[0] \t p_down[1] \t ke_up \t ke_down \t \
+  fprintf(file, "t \t pos_up \t pos_down \t \
+      mom_up \t mom_down \t ke_up \t ke_down \t \
       e_up \t e_down \t mass_up \t mass_down \n");
 
   for (int itr=0; itr < (int)((t*1.)/dt); itr++){
 
-    struct Particle *part = particles;
-    for(int i=0; i<npart; i++, part++){
+    struct Particle *part = particles; // come up with a better structure than a linked list
+
+    for(long int i=0; i<npart; i++, part++){
       // 2) STEP SOLUTION IN TIME 
       solver->func_dostep(solver, part, pot);
       // 3) UPDATE PARTICLE INFORMATION 
@@ -109,17 +103,15 @@ int main(int argc, char *argv[]){
       // 4) CALL HOPPER 
       hopper->func_hop(part, hopper, pot, solver);
     }
-    if(itr % (int)(((t*1.)/dt)/80) == 0){
+    if(itr % (int)(((t*1.)/dt)/40) == 0){
       sh_observables_update(observables, particles);
-      fprintf(file, "%d \t %.17g \t %.17g \t %.17g \t %.17g \t  %.17g \
-                         \t %.17g \t %.17g \t %.17g \t %.17g \t %.17g \
+      fprintf(file, "%.17g \t %.17g \t %.17g \t  %.17g \
+                          \t %.17g \t %.17g \t %.17g \
                           \t %.17g \t %.17g \t %.17g \
                           \t %.17g \n",
-          itr,
-          observables->x_up[0], observables->x_up[1],
-          observables->x_down[0], observables->x_down[1],
-          observables->p_up[0], observables->p_up[1],
-          observables->p_down[0], observables->p_down[1],
+          itr * dt,
+          observables->x_up[0], observables->x_down[0],
+          observables->p_up[0], observables->p_down[0],
           observables->ke_up, observables->ke_down,
           observables->e_up, observables->e_down,
           observables->mass_up, observables->mass_down
@@ -130,3 +122,4 @@ int main(int argc, char *argv[]){
   fclose(file);
   return 0;
 }
+
