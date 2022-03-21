360bb1c970ee7b12aa8aa507152d0d6dc8a68057
Michael <mir00021@hotmail.com>
2022-03-21 16:25:11 +0000

    need integration for hagedorn method

--- src/Odeint/odeint.c
+++ src/Odeint/odeint.c
@@ -8,11 +8,11 @@ struct Odeint *odeint_new(double t, double dt,
   odeint->t = t;
   odeint->dt = dt;
   odeint->dim = dim;
-  if (!strcmp(solver, "euler_symplectic")){
-    odeint->func_dostep = odeint_solver_eulsym;
-  }
-  else if (!strcmp(solver, "lietrotter_symplectic")){
-    odeint->func_dostep = odeint_solver_lietrotter;
-  }
+  //if (!strcmp(solver, "euler_symplectic")){
+  //  odeint->func_dostep = odeint_solver_eulsym;
+  //}
+  //else if (!strcmp(solver, "lietrotter_symplectic")){
+  //  odeint->func_dostep = odeint_solver_lietrotter;
+  //}
   return odeint;
 }
